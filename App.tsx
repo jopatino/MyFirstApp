8aaaea70e733cedf3a5aa6ae4cadec4338dbcb0b
jopatino <jopatino86@misena.edu.co>
2024-04-24 17:19:18 -0500

    Added new icons package - Added floating action button

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { SafeAreaView, Text, View } from "react-native"
 import { HelloWorldScreen } from "./src/presentation/screens/HelloWorldScreen"
 import { CounterScreen } from "./src/presentation/screens/CounterScreen"
 import { PaperProvider } from "react-native-paper"
+import { CounterM3Screen } from "./src/presentation/screens/CounterM3Screen"
 
 // View es un div y TEXT es un texo
 export const App = () => {
@@ -9,7 +10,8 @@ export const App = () => {
     <PaperProvider>
       <SafeAreaView style={{ flex: 1 }}>
         {/* <HelloWorldScreen name="Jose Oliverio Patiño" /> */}
-        <CounterScreen></CounterScreen>
+        {/* <CounterScreen></CounterScreen> */}
+        <CounterM3Screen></CounterM3Screen>
       </SafeAreaView>
     </PaperProvider>
   )
---
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import {  Text, View } from "react-native"
+import { globalStyles } from "../themes/global.styles";
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import { FAB } from "react-native-paper";
+
+export const CounterM3Screen = () => {
+    
+    const [count, setcount] = useState(10);
+
+    return (
+    <View style={globalStyles.centerContainer}>
+        <Text style={globalStyles.title}>{count}</Text>
+
+        
+        <FAB
+            // icon="plus"
+            label="+1"
+            style={globalStyles.fab}
+            onPress={() => setcount(count + 1)}
+            onLongPress={() => setcount(0)}
+            // icon={ () => <Icon name="accessibility-outline" size={ 25 }></Icon>}
+        />
+    </View>
+    )
+}
